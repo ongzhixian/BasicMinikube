821e680a43798262068126046b28d8c692efca32
ongzhixian <zhixian@hotmail.com>
2024-08-26 21:43:31 +0800

    Add read from topic beginning

--- KafkaConsumerConsoleApp/Program.cs
+++ KafkaConsumerConsoleApp/Program.cs
@@ -5,16 +5,33 @@ var config = new ConsumerConfig
     BootstrapServers = "localhost:9092",
     GroupId = "test-group",
     AutoOffsetReset = AutoOffsetReset.Earliest
+
+    //EnableAutoCommit = false,
+    //StatisticsIntervalMs = 5000,
+    //SessionTimeoutMs = 6000,
+    //AutoOffsetReset = AutoOffsetReset.Earliest,
+    //EnablePartitionEof = true,
+    //// A good introduction to the CooperativeSticky assignor and incremental rebalancing:
+    //// https://www.confluent.io/blog/cooperative-rebalancing-in-kafka-streams-consumer-ksqldb/
+    //PartitionAssignmentStrategy = PartitionAssignmentStrategy.CooperativeSticky
 };
 
-using var consumer = new ConsumerBuilder<Ignore, string>(config).Build();
+using var consumer = new ConsumerBuilder<Ignore, string>(config)
+    .Build();
+
+TopicPartitionOffset tps = new TopicPartitionOffset(
+    new TopicPartition("test-topic", 0)
+    , Offset.Beginning);
+
+consumer.Assign(tps);
+
 consumer.Subscribe("test-topic");
 try
 {
     while (true)
     {
         var message = consumer.Consume();
-        Console.WriteLine($"Received message: {message.Value}");
+        Console.WriteLine($"Received message: {message.Message.Value}");
     }
 }
 catch (Exception ex)
