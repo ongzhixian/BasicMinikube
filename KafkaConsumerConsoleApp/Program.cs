c99fde815c7979deb3ac5ab510ec0384ea577d1a
ongzhixian <zhixian@hotmail.com>
2024-08-26 21:14:43 +0800

    Add sample Kafka apps

---
+++ KafkaConsumerConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using Confluent.Kafka;
+
+var config = new ConsumerConfig
+{
+    BootstrapServers = "localhost:9092",
+    GroupId = "test-group",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+using var consumer = new ConsumerBuilder<Ignore, string>(config).Build();
+consumer.Subscribe("test-topic");
+try
+{
+    while (true)
+    {
+        var message = consumer.Consume();
+        Console.WriteLine($"Received message: {message.Value}");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
---
+++ KafkaProducerConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using Confluent.Kafka;
+
+var config = new ProducerConfig
+{
+    BootstrapServers = "localhost:9092",
+    //LingerMs = 0,
+    //Acks = 0,
+};
+
+var topic = "test-topic";
+
+using var producer = new ProducerBuilder<Null, string>(config).Build();
+
+for (int i = 0; i < args.Length; i++)
+{
+    var message = new Message<Null, string>
+    {
+        Value = args[i]
+    };
+
+    producer.Produce(
+        topic
+        , message
+        , deliveryReport => {
+            Console.WriteLine(deliveryReport.Message.Value);
+        });
+
+    producer.Flush();
+}
+
