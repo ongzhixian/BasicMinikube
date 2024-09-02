63c40cca9ae246694f24c11ad7b078dda891dfae
ongzhixian <zhixian@hotmail.com>
2024-09-02 23:27:05 +0800

    Add user secrets sync app (work in progress)

---
+++ UserSecretsSyncApp/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.Extensions.Configuration;
+
+//
+// %APPDATA%\Microsoft\UserSecrets\<user_secrets_id>\secrets.json
+
+//Microsoft.Extensions.Configuration.UserSecretsConfigurationExtensions.AddUserSecrets()
+
+IConfiguration Configuration = new ConfigurationBuilder()
+   .AddUserSecrets("47df7034-c1c1-4b87-8373-89f5e42fc9ec")
+   .Build();
+
+
+Console.WriteLine("All done");
