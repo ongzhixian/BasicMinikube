d35c3f18cf0b118649f6e5d3a93195b6b1b1e91d
ongzhixian <zhixian@hotmail.com>
2024-09-08 13:18:08 +0800

    Add a dummy WorkInProgressAttribute

---
+++ WareLogix/WorkInProgressAttribute.cs
@@ -0,0 +1,20 @@
+﻿namespace WareLogix;
+
+
+[AttributeUsage(
+    AttributeTargets.Class 
+    | AttributeTargets.Struct
+    | AttributeTargets.Method)
+]
+public class WorkInProgressAttribute : Attribute
+{
+    private string Name;
+    public double Version;
+
+
+    public WorkInProgressAttribute(string name)
+    {
+        Name = name;
+        Version = 1.0;
+    }
+}
