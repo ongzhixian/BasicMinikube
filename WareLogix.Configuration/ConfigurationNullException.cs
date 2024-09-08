012c5fccd3eb696eb1fa386d827f58d90b32fbf7
ongzhixian <zhixian@hotmail.com>
2024-09-08 21:13:41 +0800

    Move ConfigurationNullException to WareLogix.Configuration

---
+++ WareLogix.Configuration/ConfigurationNullException.cs
@@ -0,0 +1,12 @@
+﻿namespace WareLogix.Configuration;
+
+//public class ConfigurationNullException : ArgumentNullException
+//{
+//    public ConfigurationNullException()
+//    {
+//    }
+
+//    public ConfigurationNullException(string? paramName) : base(paramName)
+//    {
+//    }
+//}
