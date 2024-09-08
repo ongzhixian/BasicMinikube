c3ddcdf70184631485189b836e8cc29d95889ae2
ongzhixian <zhixian@hotmail.com>
2024-09-08 21:15:07 +0800

    Update TechnicalAnalysisConsoleApp

---
+++ TechnicalAnalysisConsoleApp/Program.cs
@@ -0,0 +1,146 @@
+﻿using System.Data.Common;
+using System;
+using System.Text;
+using System.Threading.Channels;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+using Serilog;
+using TechnicalAnalysisConsoleApp.Services;
+using WareLogix.Messaging.RabbitMq;
+using WareLogix.Messaging;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Warning()
+    .WriteTo.Console()
+    .WriteTo.File("logs/root.logs", rollingInterval: RollingInterval.Day)
+    .CreateBootstrapLogger();
+
+Log.Debug("TechnicalAnalysisConsoleApp bootstrapped");
+
+try
+{
+    HostApplicationBuilder builder = GetApplicationBuilder(args);
+
+    //Serilog.Sinks.SystemConsole.Themes.AnsiConsoleTheme.Literate
+    //Log.Debug($"USE_DUMMY_FLAG {builder.Configuration["FeatureFlags:USE_DUMMY_FLAG"]}");
+    //Log.Debug($"OandaAccessToken {builder.Configuration["OandaAccessToken"]}");
+    Log.Debug("=== Dumping configuration ===");
+    Log.Debug("Key                     Value");
+    Log.Debug($"Oanda:RestApiUrl        {builder.Configuration["Oanda:RestApiUrl"]}");
+    Log.Debug($"Oanda:StreamingApiUrl   {builder.Configuration["Oanda:StreamingApiUrl"]}");
+
+    ConfigureLogging(builder);
+
+    //builder.Services.AddOptions<MyOptions>().BindConfiguration("MyConfig");
+
+    //AddHttpResilience(builder);
+
+    //builder.Services.AddSingleton<OandaService>();
+
+    //AddHttpServices(builder);
+
+    builder.Services.AddSingleton<IMessageQueueConsumer, RabbitMqConsumer>(sp =>
+    {
+        return new RabbitMqConsumer(builder.Configuration);
+    });
+
+    //builder.Services.AddHostedService<Worker>();
+    builder.Services.AddHostedService<CloudAmqpDemoService>();
+
+    using (IHost host = builder.Build())
+    {
+        host.Run();
+    }
+}
+catch (Exception ex)
+{
+    Log.Error(ex, "Unhandled exception!");
+}
+finally
+{
+    await Log.CloseAndFlushAsync();
+}
+
+static HostApplicationBuilder GetApplicationBuilder(string[] args)
+{
+    // What `CreateApplicationBuilder` does:
+    // Sets the content root to the path returned by GetCurrentDirectory().
+    // Loads host configuration from:
+    //   Environment variables prefixed with DOTNET_.
+    //   Command - line arguments.
+    // Loads app configuration from:
+    //   appsettings.json.
+    //   appsettings.{ Environment}.json.
+    //   Secret Manager when the app runs in the Development environment.
+    //   Environment variables.
+    //   Command - line arguments.
+    // Adds the following logging providers:
+    //   Console
+    //   Debug
+    //   EventSource
+    //   EventLog(only when running on Windows)
+    // Enables scope validation and dependency validation when the environment is Development.
+    // Reference: https://learn.microsoft.com/en-us/dotnet/core/extensions/generic-host?tabs=appbuilder
+
+    Log.Debug("Create application builder");
+    return Host.CreateApplicationBuilder(args);
+}
+
+static void ConfigureLogging(HostApplicationBuilder builder)
+{
+    builder.Logging.ClearProviders();
+
+    builder.Logging.AddSerilog(new LoggerConfiguration()
+        .ReadFrom
+        .Configuration(builder.Configuration)
+        .CreateLogger());
+
+    Log.Debug("Logging configured");
+}
+
+
+// METHODS TO DUMP TO ConnectHttp class library
+
+//static void AddHttpResilience(HostApplicationBuilder builder)
+//{
+//    // HTTP Resiliency
+//    // Reference: https://learn.microsoft.com/en-us/dotnet/core/resilience/?tabs=dotnet-cli
+
+//    const string resiliencePipelineName = "Retry-Timeout";
+
+//    builder.Services.AddResiliencePipeline(resiliencePipelineName, static builder =>
+//    {
+//        // See: https://www.pollydocs.org/strategies/retry.html
+//        builder.AddRetry(new RetryStrategyOptions
+//        {
+//            ShouldHandle = new PredicateBuilder().Handle<TimeoutRejectedException>()
+//        });
+
+//        // See: https://www.pollydocs.org/strategies/timeout.html
+//        builder.AddTimeout(TimeSpan.FromSeconds(1.5));
+//    });
+
+//    builder.Services.AddResilienceEnricher();
+//}
+
+//static void AddHttpServices(HostApplicationBuilder builder)
+//{
+//    builder.Services.AddHttpClient();
+
+//    // Reference: https://learn.microsoft.com/en-us/dotnet/core/resilience/http-resilience?tabs=dotnet-cli
+//    builder.Services
+//        .AddHttpClient<OandaService>()
+//        .AddStandardResilienceHandler(options =>
+//        {
+//            options.Retry.MaxRetryAttempts = 3;
+//        });
+
+//    // See also the many fun topics about HTTP, eg.
+//    // https://learn.microsoft.com/en-us/dotnet/core/extensions/httpclient-sni
+//}
---
+++ TechnicalAnalysisConsoleApp/Services/CloudAmqpDemoService.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+
+using WareLogix.Messaging;
+
+namespace TechnicalAnalysisConsoleApp.Services;
+
+internal class CloudAmqpDemoService : BackgroundService
+{
+    private readonly ILogger<CloudAmqpDemoService> logger;
+
+    private readonly IMessageQueueConsumer messageQueueConsumer;
+
+    public CloudAmqpDemoService(ILogger<CloudAmqpDemoService> logger, IMessageQueueConsumer messageQueueConsumer)
+    {
+        this.logger = logger;
+        this.messageQueueConsumer = messageQueueConsumer;
+    }
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+
+        //int i = 0;
+        //while (!stoppingToken.IsCancellationRequested)
+        //{
+        //    messageQueueConsumer.ConsumeFromQueue("queue1");
+        //    //messageQueuePublisher.PublishToQueue("queue1", $"Message {i} - {DateTime.Now:O}");
+
+        //    //await Task.Delay(3600, stoppingToken);
+        //}
+
+        return Task.Run(() =>
+        {
+            messageQueueConsumer.ConsumeFromQueue("queue1");
+        });
+
+
+        //// add the message receive event
+        //consumer.Received += (model, deliveryEventArgs) =>
+        //{
+        //    var body = deliveryEventArgs.Body.ToArray();
+        //    // convert the message back from byte[] to a string
+        //    var message = Encoding.UTF8.GetString(body);
+        //    Console.WriteLine("** Received message: {0} by Consumer thread **", message);
+        //    // ack the message, ie. confirm that we have processed it
+        //    // otherwise it will be requeued a bit later
+        //    _channel.BasicAck(deliveryEventArgs.DeliveryTag, false);
+        //};
+
+        //// start consuming
+        //_ = _channel.BasicConsume(consumer, queueName);
+        //// Wait for the reset event and clean up when it triggers
+        ////_resetEvent.WaitOne();
+        //_channel?.Close();
+        //_channel = null;
+        //_connection?.Close();
+        //_connection = null;
+    }
+}
