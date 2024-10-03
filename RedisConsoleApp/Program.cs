3c073614bfdb86ac2d3803d31fbb9c1ec76e08f0
ongzhixian <zhixian@hotmail.com>
2024-10-03 23:21:17 +0800

    Move Jenkinsfile

--- RedisConsoleApp/Program.cs
+++ RedisConsoleApp/Program.cs
@@ -4,12 +4,14 @@ using StackExchange.Redis;
 
 // http://172.20.39.125:30228
 
-ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("172.20.39.125:30228");
+//ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("172.20.39.125:30228");
+//ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("redis");
+ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost:26379");
 IDatabase db = redis.GetDatabase();
 
 
-//db.StringSet("foo", "bar");
-Console.WriteLine(db.StringGet("foo1")); // prints bar
+db.StringSet("foo", "bar");
+Console.WriteLine(db.StringGet("foo")); // prints bar
 
 //Console.WriteLine(db.StringGet("gaga")); // prints bar
 
