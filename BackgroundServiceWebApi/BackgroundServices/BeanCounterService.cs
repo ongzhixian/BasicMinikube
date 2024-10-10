5b9a39ee95e326083dee6360e9d31da2ad38bbe2
ongzhixian <zhixian@hotmail.com>
2024-10-10 13:25:49 +0800

    Add BackgroundServiceWebApi

---
+++ BackgroundServiceWebApi/BackgroundServices/BeanCounterService.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace BackgroundServiceWebApi.BackgroundServices;
+
+public class BeanCounterService : BackgroundService
+{
+    private readonly ILogger<BeanCounterService> logger;
+
+    public BeanCounterService(ILogger<BeanCounterService> logger)
+    {
+        this.logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        int i = 0;
+
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            Console.WriteLine($"BeanCounterService {i++}");
+
+            await Task.Delay(5000, stoppingToken);
+        }
+    }
+}
