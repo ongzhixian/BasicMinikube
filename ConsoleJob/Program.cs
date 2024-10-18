e5e87bc8b4177ec6cc47723fcbc32d32dc87f072
ongzhixian <zhixian@hotmail.com>
2024-10-18 13:27:20 +0800

    Add code to read appsettings.json

--- ConsoleJob/Program.cs
+++ ConsoleJob/Program.cs
@@ -1,6 +1,25 @@
-﻿for (var i = 0; i < 10; i++)
+﻿using System.Reflection;
+
+var assemblyLocation = Assembly.GetExecutingAssembly().Location;
+var assemblyDirectory = Path.GetDirectoryName(assemblyLocation) ?? throw new Exception($"No directory name for {assemblyLocation}");
+
+Console.WriteLine("[PROGRAM START]");
+Console.WriteLine($"Runtime directory is [{Environment.CurrentDirectory}]");
+Console.WriteLine($"  Executable path is [{assemblyLocation}]");
+Console.WriteLine($"  Executable dir  is [{assemblyDirectory}]");
+
+var appsettingsJsonPath = Path.Combine(assemblyDirectory, "appsettings.json");
+if (File.Exists(appsettingsJsonPath))
+{
+    using StreamReader reader = new(appsettingsJsonPath);
+    string contents = reader.ReadToEnd();
+    Console.WriteLine(contents);
+}
+
+for (var i = 0; i < 10; i++)
 {
     Thread.Sleep(3000);
     Console.WriteLine($"Hello, World announced at {DateTime.Now:O}");
 }
 
+Console.WriteLine("[PROGRAM END]");
