52bd9d3f9b22d8de8e395ebff6ca693ecec8e386
ongzhixian <zhixian@hotmail.com>
2024-10-20 08:57:37 +0800

    Add LeasedLockConsoleApp

---
+++ LeaseLockConsoleApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using k8s;
+using k8s.LeaderElection.ResourceLock;
+using k8s.LeaderElection;
+using k8s.Models;
+
+KubernetesClientConfiguration config;
+
+if (Environment.GetEnvironmentVariables().Contains("KUBERNETES_SERVICE_HOST"))
+    config = KubernetesClientConfiguration.InClusterConfig();
+else
+    config = KubernetesClientConfiguration.BuildConfigFromConfigFile();
+
+IKubernetes client = new Kubernetes(config);
+
+
+var leaseName = "example-lease2";
+var namespaceName = "default";
+var identity = Guid.NewGuid().ToString();
+var leaseDuration = TimeSpan.FromSeconds(15);
+var renewDeadline = TimeSpan.FromSeconds(10);
+var retryPeriod = TimeSpan.FromSeconds(2);
+var machineName = Environment.MachineName;
+identity = machineName;
+var leaderWins = 0;
+
+var leaderLock = new LeaseLock(client, namespaceName, leaseName, identity);
+
+var leaderElectionConfig = new LeaderElectionConfig(leaderLock)
+{
+    LeaseDuration = TimeSpan.FromSeconds(15),
+    RenewDeadline = TimeSpan.FromSeconds(10),
+    RetryPeriod = TimeSpan.FromSeconds(2)
+};
+var leaderElector = new LeaderElector(leaderElectionConfig);
+leaderElector.OnStartedLeading += () => Console.WriteLine("Got Leader Lock");
+leaderElector.OnNewLeader += leader => Console.WriteLine("Leader changed to: {leader}", leader);
+leaderElector.OnStoppedLeading += () => Console.WriteLine("Lost Leader Lock");
+
+
+while (true)
+{
+    // Scenarios
+    // 1.   Lease does not exists               -- create lease and be leader
+    // 2.   Lease exists but has expired        -- declare leader
+    // 3a.  Lease exists and has not expired    -- if is  holder, renew lease
+    // 3b.  Lease exists and has not expired    -- if not holder, just cry
+    await leaderElector.RunUntilLeadershipLostAsync();
+
+    await Task.Delay(retryPeriod);
+}
---
+++ LeaseLockConsoleApp/mk.ps1
@@ -0,0 +1,93 @@
+# Script to "make" assembly
+param(
+	[switch]$Debug,
+	[string]$versionFilePath = "./version.txt"
+)
+
+$projectName = "LeaseLockConsoleApp"
+
+$main = {
+
+	Push-Location
+	Set-Location $PSScriptRoot
+
+	Write-Debug "versionFilePath is: $versionFilePath" 
+
+	$lastVersion = Get-LastVersion $versionFilePath
+	$nextVersion = Update-Version $lastVersion
+
+	Write-Host "Building version: [$nextVersion]"
+	
+	Build-Assembly $nextVersion
+	
+	#Build-NugetPackage $nextVersion
+
+	#$nugetPackagePath = ".\out\$projectName.$nextVersion.nupkg"
+	#PublishTo-NugetOrg $nugetPackagePath
+	#PublishTo-LocalFeed $nugetPackagePath
+	
+	$nextVersion.ToString() | Out-File $versionFilePath
+
+	Pop-Location
+}
+
+
+# SCRIPT HELPER FUNCTIONS
+
+## DOTNET FUNCTIONS
+
+Function Build-Assembly($version) {
+	dotnet restore 
+	dotnet build -p:AssemblyVersion=$version --nologo --no-restore --configuration Release
+}
+
+Function Build-NugetPackage($version) {
+	Write-Host "Build-NugetPackage"
+	dotnet pack -p:PackageVersion=$version --output out --nologo --no-restore --no-build --configuration Release
+}
+
+Function PublishTo-NugetOrg($packagePath) {
+	$apiKey = $($(dotnet user-secrets list --json ) -replace '//(BEGIN|END)' | ConvertFrom-Json)."nuget-warelogix-api-key"
+	dotnet nuget push $packagePath --source https://api.nuget.org/v3/index.json --api-key $apiKey
+}
+
+Function PublishTo-LocalFeed($packagePath) {
+	nuget add $packagePath -Source D:\nuget-repository
+}
+
+
+## VERSIONING FUNCTIONS
+
+Function Get-LastVersion($versionFilePath) {
+	
+	if (Test-Path $versionFilePath) {
+		$versionText = Get-Content $versionFilePath
+		Write-Debug "$versionFilePath = `"$versionText`""
+
+		try	{
+			$lastVersion = [Version]::new($versionText)
+		}
+		catch [System.ArgumentException], [System.FormatException] {
+			$lastVersion = [version]::new("0.0")
+		}
+		catch {
+			Write-Host $_.Exception
+		}
+	}
+
+	Write-Debug "Last version was $lastVersion"
+	return $lastVersion
+}
+
+
+Function Update-Version($lastVersion) {
+	
+	$nextVersion = [version]::new($lastVersion.Major, $lastVersion.Minor, $lastVersion.Build + 1)
+	
+	Write-Debug "Next version will be $nextVersion"
+	return $nextVersion
+}
+
+# MAIN
+
+& $main
