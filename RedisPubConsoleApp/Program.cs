16bed9027998dcc87e118c4fc04a07b3e3722384
ongzhixian <zhixian@hotmail.com>
2024-11-13 00:17:23 +0800

    Add Redis Pub and Sub console apps

---
+++ RedisPubConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using StackExchange.Redis;
+
+string redisConnectionString = "localhost:26379";
+string redisTestListName = "TestList";
+
+ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(redisConnectionString);
+IDatabase db = redis.GetDatabase();
+ISubscriber subscriber = redis.GetSubscriber();
+RedisChannel testListChannel = new("TestListChannel", RedisChannel.PatternMode.Literal);
+
+for (int i = 0; i < 9999; i++)
+{
+    //long listLength = await db.ListRightPushAsync(redisTestListName, new RedisValue("Test message"));
+    
+    //Console.WriteLine($"New list length is {listLength}");
+
+    long receipients = await subscriber.PublishAsync(testListChannel, new RedisValue($"My message number {i}"));
+
+    Console.WriteLine($"Message sent to {receipients} receipient(s)");
+
+    await Task.Delay(1000 * 10);
+}
---
+++ RedisSubConsoleApp/Program.cs
@@ -0,0 +1,51 @@
+﻿using StackExchange.Redis;
+
+string redisConnectionString = "localhost:26379";
+string redisTestListName = "TestList";
+
+CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+CancellationToken cancellationToken = cancellationTokenSource.Token;
+
+
+ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(redisConnectionString);
+ISubscriber subscriber = redis.GetSubscriber();
+RedisChannel testListChannel = new("TestListChannel", RedisChannel.PatternMode.Literal);
+
+ChannelMessageQueue channelMessageQueue = await subscriber.SubscribeAsync(testListChannel);
+channelMessageQueue.OnMessage(ChannelMessageHandler);
+
+Console.CancelKeyPress += Console_CancelKeyPress;
+
+void Console_CancelKeyPress(object? sender, ConsoleCancelEventArgs e)
+{
+    cancellationTokenSource.Cancel();
+    e.Cancel = true;
+    
+}
+
+void ChannelMessageHandler(ChannelMessage channelMessage)
+{
+    Console.WriteLine(channelMessage.Message);
+}
+
+
+Console.WriteLine("[PROGRAM START]");
+
+try
+{
+    while (!cancellationToken.IsCancellationRequested)
+    {
+        TimeSpan pingLatency = subscriber.Ping();
+
+        Console.WriteLine($"Ping latency is {pingLatency}");
+
+        await Task.Delay(1000 * 3, cancellationToken);
+    }
+}
+catch (TaskCanceledException)
+{
+    Console.WriteLine("AAA");
+    //throw;
+}
+
+Console.WriteLine("[PROGRAM END]");
