9c057f4125bc713ebc88bb2a203f989dc291228a
ongzhixian <zhixian@hotmail.com>
2024-11-17 08:44:21 +0800

    Add Correlation ID middleware

---
+++ SimpleWebApi/Extensions/IApplicationBuilderExtensions.cs
@@ -0,0 +1,11 @@
+﻿using SimpleWebApi.Middleware;
+
+namespace SimpleWebApi.Extensions;
+
+public static class IApplicationBuilderExtensions
+{
+    public static IApplicationBuilder UseCorrelationIdMiddleware(this IApplicationBuilder applicationBuilder)
+    {
+        return applicationBuilder.UseMiddleware<CorrelationIdMiddleware>();
+    }
+}
---
+++ SimpleWebApi/Middleware/CorrelationIdMiddleware.cs
@@ -0,0 +1,22 @@
+﻿namespace SimpleWebApi.Middleware;
+
+public class CorrelationIdMiddleware(RequestDelegate next)
+{
+    private const string CorrelationIdHeaderKey = "X-Correlation-ID";
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        if (!context.Request.Headers.ContainsKey(CorrelationIdHeaderKey))
+            context.Request.Headers.Append(CorrelationIdHeaderKey, Guid.NewGuid().ToString("N"));
+
+        var correlationId = context.Request.Headers[CorrelationIdHeaderKey];
+
+        //
+        //using (LogContext.PushProperty("CorrelationId", correlationId))
+        //{
+        //    await next(context);
+        //}
+
+        await next(context);
+    }
+}
