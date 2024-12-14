78d39cf3edc3946990a102d32808a50b58b1161d
ongzhixian <zhixian@hotmail.com>
2024-12-14 09:04:53 +0800

    Add S3Models

---
+++ WareLogix.Models.S3Models/README.txt
@@ -0,0 +1,5 @@
+XSD is sourced from:
+https://doc.s3.amazonaws.com/2006-03-01/AmazonS3.xsd
+
+AmazonS3 classes generated from XSD file using xsd.exe
+xsd /classes AmazonS3.xsd
---
+++ WareLogix.Models.S3Models/S3XmlSerializer.cs
@@ -0,0 +1,50 @@
+﻿using System.Xml;
+using System.Xml.Serialization;
+
+namespace WareLogix.Models.S3Models;
+
+/// <summary>
+/// TODO: WareLogix XML library
+/// </summary>
+public class S3XmlSerializer
+{
+    private static readonly XmlSerializerNamespaces emptySerializerNamespaces = new([XmlQualifiedName.Empty]);
+
+    const string S3XmlNamespace = "http://s3.amazonaws.com/doc/2006-03-01/";
+
+    public static string ToXml<T>(T sourceObject) where T : new()
+    {
+        try
+        {
+            using StringWriter sw = new();
+            var xmlSerializer = new XmlSerializer(typeof(T), defaultNamespace: S3XmlNamespace);
+            xmlSerializer.Serialize(sw, sourceObject, emptySerializerNamespaces);
+            return sw.ToString();
+        }
+        catch (Exception)
+        {
+            return string.Empty;
+        }
+
+        //using MemoryStream ms = new();
+        //return System.Text.Encoding.UTF8.GetString(ms.ToArray());
+    }
+
+    public static T? FromXml<T>(string xml) where T : class
+    {
+        try
+        {
+            using var xmlStringReader = new StringReader(xml);
+            var xmlSerializer = new XmlSerializer(typeof(T), defaultNamespace: S3XmlNamespace);
+            return xmlSerializer.Deserialize(xmlStringReader) as T;
+        }
+        catch (Exception)
+        {
+            return default;
+        }
+
+        //using MemoryStream ms = new();
+        //xmlSerializer.Serialize(ms, typeof(T), defaultSerializerNamespaces);
+        //return System.Text.Encoding.UTF8.GetString(ms.ToArray());
+    }
+}
