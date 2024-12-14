e2820a59eab2c5c297dd09981de4c302d62f1e4e
ongzhixian <zhixian@hotmail.com>
2024-12-14 09:39:14 +0800

    Add stub for WareLogix.Protocol

---
+++ WareLogix.Protocol/Class1.cs
@@ -0,0 +1,8 @@
+﻿namespace WareLogix.Protocol;
+
+public class Class1
+{
+    // Implement mime (Multipurpose Internet Mail Extensions) resolver
+    // See C:\Program Files\IIS Express\AppServer\applicationhost.config
+    // Look for staticcontent/mimetypes
+}
