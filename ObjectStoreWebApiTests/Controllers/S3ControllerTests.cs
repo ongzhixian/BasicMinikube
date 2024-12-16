fa1fee688ca7a475c46a2d607a7a734a5efa9adc
ongzhixian <zhixian@hotmail.com>
2024-12-16 23:32:01 +0800

    add tests

---
+++ ObjectStoreWebApiTests/Controllers/S3ControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+
+using Moq;
+
+using ObjectStoreWebApi.Services;
+
+namespace ObjectStoreWebApi.Controllers.Tests;
+
+[TestClass()]
+public class S3ControllerTests
+{
+    private readonly Mock<ILogger<S3Controller>> logger = new();
+    private readonly Mock<IObjectStoreService> objectStoreService = new();
+
+    public S3ControllerTests()
+    {
+    }
+
+    
+
+    public void GetBucketListTest()
+    {
+        objectStoreService.Setup(m => m.ListBuckets());
+
+        S3Controller controller = new S3Controller(logger.Object, objectStoreService.Object);
+
+        controller.GetBucketList();
+
+        objectStoreService.Verify(mock => mock.ListBuckets(), Times.Once());
+    }
+}
