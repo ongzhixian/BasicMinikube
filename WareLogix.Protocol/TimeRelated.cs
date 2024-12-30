ef40c1f545802efa710a3a00af24859aebcd4fe6
ongzhixian <zhixian@hotmail.com>
2024-12-30 14:49:41 +0800

    Add protocol

---
+++ WareLogix.Protocol/TimeRelated.cs
@@ -0,0 +1,19 @@
+﻿namespace WareLogix.Protocol;
+
+internal sealed class TimeRelated
+{
+    static readonly TimeZoneInfo Est = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
+
+    public static DateTime EstNow
+    {
+        get
+        {
+            return TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, Est);
+        }
+    }
+
+    public static DateTime GetEstFromUtc(DateTime date)
+    {
+        return TimeZoneInfo.ConvertTimeFromUtc(date, Est);
+    }
+}
